04f0ca38e29f00efa3d24cebce65fa9055502ba0
cagdaskasimoglu <caadas@gmail.com>
2020-09-10 05:56:57 +0300

    - Minor fixes

--- assets/js/main-login.js
+++ assets/js/main-login.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
+
+    $('.slider-container').on('init', function(event, slick, currentSlide, nextSlide){
+       $(this).addClass('shown');
+    });
     $('.slider-container').slick();
 });
